653e50b2832a23288f683c6d8bf35e8f1b952998
José Henrique da Silva guimarães <josehenriquesg@hotmail.com>
2021-01-14 16:39:04 -0300

    Adicionar arquivos de projeto.

---
+++ CursoSesprosp/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CursoSesprosp.Models
+{
+    public class Context : DbContext
+    {
+
+        public DbSet<Categoria> categorias { get; set;}
+        //responsável por configurar o Entity framework
+        public DbSet<Produto> Produtos { get; set;}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\mssqllocaldb;Database=CursoSesprosp;Integrated Security=True");
+
+    }
+}
